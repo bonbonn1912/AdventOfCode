c367ac3bc75067493604562ab1e74660fa5af2ff
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2021-12-03 19:08:42 +0100

    refactored part2

--- Day03/Part2.java
+++ Day03/Part2.java
@@ -15,7 +15,6 @@ public class Part2 {
     public static LinkedList<String> toBeRemoved2 = new LinkedList<String>();
 
     public static void main(String[] args) throws Exception {
-
         File input = new File("PATH TO INPUT TXT FILE");
 
         try (BufferedReader br = new BufferedReader(new FileReader(input))) {
@@ -25,41 +24,28 @@ public class Part2 {
                 number2List.add(line);
             }
         }
-        System.out.println(createFirstNumber(number1List, toBeRemoved1) * createSecondNumber(number2List, toBeRemoved2));
+        System.out.println(createNumber(number1List, toBeRemoved1, 1) * createNumber(number2List, toBeRemoved2, 2));
     }
-
-    public static int createFirstNumber(LinkedList<String> inputList1, LinkedList<String> toBeRemoved1) {
+    public static int createNumber(LinkedList<String> inputList1, LinkedList<String> toBeRemoved1, int digit) {
         int i = 0;
         while (inputList1.size() != 1) {
-            char mostCommonBit = createMostCommonBit(inputList1, i);
-            createUselessInputs(inputList1, mostCommonBit, i, toBeRemoved1);
-            removeEntries(inputList1, toBeRemoved1);
+            if (digit == 1) {
+                char mostCommonBit = createMostCommonBit(inputList1, i);
+                createUselessInputs(inputList1, mostCommonBit, i, toBeRemoved1);
+                removeEntries(inputList1, toBeRemoved1);
+            } else {
+                char LessCommonBit = createLessCommonBit(inputList1, i);
+                createUselessInputs(inputList1, LessCommonBit, i, toBeRemoved1);
+                removeEntries(inputList1, toBeRemoved1);
+            }
             i++;
         }
         int decimal = 0;
         for (String rest : inputList1) {
             decimal = Integer.parseInt(rest, 2);
-
         }
         return decimal;
     }
-
-    public static int createSecondNumber(LinkedList<String> inputList2, LinkedList<String> toBeRemoved2) {
-        int i = 0;
-        while (inputList2.size() != 1) {
-            char LessCommonBit = createLessCommonBit(inputList2, i);
-            createUselessInputs(inputList2, LessCommonBit, i, toBeRemoved2);
-            removeEntries(inputList2, toBeRemoved2);
-            i++;
-        }
-
-        int decimal = 0;
-        for (String rest : inputList2) {
-            decimal = Integer.parseInt(rest, 2);
-        }
-        return decimal;
-    }
-
     public static char createMostCommonBit(LinkedList<String> inputMost, int index) {
         int countOne = 0;
         int countZero = 0;
@@ -79,7 +65,6 @@ public class Part2 {
             return '0';
         }
     }
-
     public static char createLessCommonBit(LinkedList<String> inputLess, int index) {
         int countOne = 0;
         int countZero = 0;
@@ -99,22 +84,17 @@ public class Part2 {
             return '1';
         }
     }
-
     public static void createUselessInputs(LinkedList<String> input, char starting, int position,
             LinkedList<String> removalList) {
         for (String entry : input) {
             if (entry.charAt(position) != starting) {
                 removalList.add(entry);
             }
-
         }
-
     }
-
     public static void removeEntries(LinkedList<String> List, LinkedList<String> toBeRemoved) {
         for (String input : toBeRemoved) {
             List.remove(input);
         }
     }
-
 }
