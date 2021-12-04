05c47845a75c3ff39652a4fbadd91df41543175d
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2021-12-04 23:30:29 +0100

    Added refactored Version for Day03/Part2

---
+++ Day03/Part2_refactored.java
@@ -0,0 +1,64 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Part2_refactored {
+
+    public static void main(String[] args) throws Exception {
+     ArrayList<String> inputList = new ArrayList<String>();
+        File input = new File("PATH TO INPUT TXT FILE");
+
+        try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                inputList.add(line);
+            }
+        }
+        System.out.println(Integer.parseInt(removeToLast(inputList,0,true),2)*Integer.parseInt(removeToLast(inputList,0,false),2));
+    }
+
+    public static int calcuteProduct(String binary_part1) {
+        String inversed = "";
+        for(char c: binary_part1.toCharArray()) {
+            if(c == '1') {
+                inversed += '0';
+            }else{
+                inversed += '1';
+            }
+        }
+        return (Integer.parseInt(inversed,2)*Integer.parseInt(binary_part1,2));
+    }
+
+    public static String removeToLast(ArrayList<String> liste, int index_, boolean mostBits) {
+        if (liste.size() != 1) {
+            if (createMostCommonBit(liste, index_)) {
+                ArrayList<String> temp2 = new ArrayList<String>(
+                        liste.stream().filter(n -> mostBits ? n.charAt(index_) == '1' : n.charAt(index_) == '0').collect(Collectors.toList()));
+                return removeToLast(temp2, index_ + 1, mostBits);
+            } else {
+                ArrayList<String> temp2 = new ArrayList<String>(liste.stream().filter(n -> mostBits ? n.charAt(index_) == '0' : n.charAt(index_) == '1').collect(Collectors.toList()));
+                return removeToLast(temp2, index_ + 1, mostBits);
+            }
+        } else {
+            return liste.get(0);
+        }
+    }
+    public static boolean createMostCommonBit(ArrayList<String> inputLess, int index) {
+        int countOne = 0;
+        int countZero = 0;
+        for (String number : inputLess) {
+            if (number.charAt(index) == '1') {
+                countOne++;
+            } else {
+                countZero++;
+            }
+        }
+        return countOne > countZero || countOne == countZero;
+    }
+    
+}
