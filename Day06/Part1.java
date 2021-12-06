e70b56a6ee242ed1a74270b95a9f4218a18e1f78
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2021-12-06 11:33:03 +0100

    Added slightly (30%) faster version to Day1 (without initiating an fish object). Added Part2 auf Day6

--- Day06/Part1.java
+++ Day06/Part1.java
@@ -9,12 +9,13 @@ import java.util.stream.Collectors;
 
 public class Part1 {
 
-    static int daysRemaining = 80;
+    static int daysRemaining = 150;
 
     public static void main(String[] args) throws Exception {
-        File input = new File("PATH TO INPUT TXT FILE");
-        LinkedList<Integer> initialStates = new LinkedList<Integer>();
+        File input = new File("D:\\Coding\\CalenderCode\\inputday6.txt");
+        LinkedList<Long> initialStates = new LinkedList<Long>();
         LinkedList<Lanternfish> fishes = new LinkedList<Lanternfish>();
+        LinkedList<Long> daysToLife = new LinkedList<Long>();
 
         try (
                 BufferedReader br = new BufferedReader(new FileReader(input))) {
@@ -23,12 +24,44 @@ public class Part1 {
             while ((line = br.readLine()) != null) {
                 String[] tempinitials = line.split(",");
                 for (String singleState : tempinitials) {
-                    initialStates.add(Integer.parseInt(singleState));
+                    initialStates.add(Long.parseLong(singleState));
+ 
+
                 }
             }
         }
-        initialStates.forEach(n -> fishes.add(new Lanternfish(n)));
-        System.out.println(decreaseLifetime(fishes));
+       initialStates.forEach(nL -> daysToLife.add(nL));
+     //  initialStates.forEach(n -> fishes.add(new Lanternfish(n)));
+        long start = System.currentTimeMillis();
+        System.out.println(decreaseLifetimeinList(daysToLife,256));
+    // System.out.println(decreaseLifetime(fishes));
+        long end = System.currentTimeMillis();
+        System.out.println(end-start);
+    }
+
+    public static int decreaseLifetimeinList(LinkedList<Long> inputList, int days){
+       // System.out.println("Dremaining: " +days);
+        if (days == 0) {
+            return inputList.size();
+        }
+        LinkedList<Long> tempList = new LinkedList<Long>();
+            while(days !=0){
+                for(Long lifetime : inputList){
+                    lifetime--;
+                    if(lifetime == -1){
+                        lifetime = 6L;
+                        tempList.add(lifetime);
+                        tempList.add(8L);    
+                    }else{
+                        tempList.add(lifetime);
+                    }
+                }
+                days--;
+               return decreaseLifetimeinList(tempList, days);
+            }
+            return 0;
+
+
     }
 
     public static int decreaseLifetime(LinkedList<Lanternfish> fishes) {
